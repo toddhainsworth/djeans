6f6a7329c8b2fc7296a6ffc275d39fd0a0e26ef7
Todd Hainsworth <hainsworth.todd@gmail.com>
2019-04-22 17:19:18 +0930

    more catalog remodelling

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'url_key', 'status', 'categories', 'price', 'special_price',)
+    list_display = ('title','url_key', 'status', 'formatted_price',)
+
+    def formatted_price(self, obj):
+        return "${}".format(obj.price)
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'url_key', 'status',)
+    list_display = ('title','url_key', 'status',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -10,30 +10,25 @@ class PriceType(Enum):
     SPECIAL = "special"
     GROUP = "group"
 
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    status = models.CharField(max_length=20, choices=[(status, status.value) for status in StatusChoice])
-    url_key = models.CharField(max_length=50) # We'll generate one if not provided
-    description = models.TextField()
-
-    def __str__(self):
-        self.title
-
-class ProductPrice(models.Model):
-    amount = models.DecimalField(decimal_places=2, max_digits=1000)
-    price_type = models.CharField(max_length=20, choices=[(price_type, price_type.value) for price_type in PriceType])
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    def __str__(self):
-        "${}".format(self.amount)
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    status = models.CharField(max_length=20, choices=[(status, status.value) for status in StatusChoice])
+    status = models.CharField(max_length=20, choices=[(status.value, status.name.capitalize()) for status in StatusChoice])
     url_key = models.CharField(max_length=50) # We'll generate one if not provided
     description = models.TextField()
 
-    products = models.ManyToManyField(Product)
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=[(status.value, status.name.capitalize()) for status in StatusChoice])
+    url_key = models.CharField(max_length=50) # We'll generate one if not provided
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=1000, default=0.0)
+    special_price = models.DecimalField(decimal_places=2, max_digits=1000, default=0.0)
+
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
-        self.title
+        return self.title
